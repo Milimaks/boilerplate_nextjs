0dde1bfd84bb8288a673f6ddbdfe78923a1bf049
Maxime Faure <maxime.faure93@gmail.com>
2025-03-20 10:13:29 +0100

    [ADD} - sql request for invoice

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -1,20 +1,35 @@
 "use server";
 
 import { z } from "zod";
+import postgres from "postgres";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
 
 const FormSchema = z.object({
   id: z.string(),
-  customer_id: z.string(),
-  amount: z.number(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
   date: z.string(),
   status: z.enum(["pending", "paid"]),
 });
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
-  const { customer_id, amount, status } = CreateInvoice.parse({
+  const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
     status: formData.get("status"),
   });
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split("T")[0];
+
+  await sql`
+  INSERT INTO invoices (customer_id, amount, status, date)
+  VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+`;
+
+  // Clear the route cache and trigger a new server request, ensuring that the displayed data is fresh.
+  revalidatePath("/dashboard/invoices");
+  redirect("dashboard/invoices");
 }

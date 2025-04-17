d9203698e8edaba893ea8f3e0fc300cffb416041
Maxime Faure <maxime.faure93@gmail.com>
2025-04-17 21:42:48 +0200

    [ADD] - data files for ProjectFile application layout

---
+++ app/data/files/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+import CleanArchitectureOverview from "@/components/features/documentation/architecture/clean-architecture-overview";
+import { links } from "@/components/features/documentation/architecture/data/navigation-data";
+import { MoreRessources } from "@/components/reusable/more-ressources";
+import { NavPagination } from "@/components/reusable/nav-pagination";
+import { externalLinks } from "./external-ressources";
+
+export default function Page() {
+  return (
+    <main className="flex flex-row justify-center">
+      <div id="content" className="max-w-4xl px-4">
+        <CleanArchitectureOverview />
+
+        <MoreRessources externalLinks={externalLinks} />
+        <NavPagination links={links} className="pt-20 pb-6" />
+      </div>
+    </main>
+  );
+}
